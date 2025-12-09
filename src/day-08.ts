6f4aacf5df7763b1e04c7ff4d95d089d5c6ccf69
Stan Smith <stan@df.com>
2025-12-08 22:26:20 -0700

    (day 8): scaffold

---
+++ src/day-08.ts
@@ -0,0 +1,30 @@
+/**
+ * Santa 🎅 wants to know what the first non-repeated letter is in a toy's name 🎁.
+ *
+ * Write a function that takes a string and returns the first letter that is not repeated, ignoring uppercase and lowercase when counting, but returning the letter as it appears in the string.
+ *
+ * If there is none, return an empty string ("").
+ */
+
+// 🧩 Examples
+findUniqueToy("Gift"); // 'G'
+// ℹ️ The G is the first letter that is not repeated
+// and we return it exactly as it appears
+
+findUniqueToy("sS"); // ''
+// ℹ️ The letters are repeated, since it doesn't distinguish uppercase
+
+findUniqueToy("reindeeR"); // 'i'
+// ℹ️ The r is repeated (even if it's uppercase)
+// and the e as well, so the first one is 'i'
+
+// More cases:
+findUniqueToy("AaBbCc"); // ''
+findUniqueToy("abcDEF"); // 'a'
+findUniqueToy("aAaAaAF"); // 'F'
+findUniqueToy("sTreSS"); // 'T'
+findUniqueToy("z"); // 'z'
+
+function findUniqueToy(toy: string): string {
+	return "";
+}
